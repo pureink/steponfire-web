651ff7560861bd96c6655962b7967f9ce673702c
pureink <44235276+pureink@users.noreply.github.com>
2022-02-26 16:12:29 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/matches/:path*',
+        destination: '/match/:path*',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
 }
 

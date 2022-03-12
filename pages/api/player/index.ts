716276da7d74d3914f1b079ee7984a37ea6e32e8
pureink <44235276+pureink@users.noreply.github.com>
2022-03-12 12:06:39 +0800

    Update index.ts

--- pages/api/player/index.ts
+++ pages/api/player/index.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 module.exports = async (req: NextApiRequest, res: NextApiResponse) => {
   let players: any = await query(escape`
-      SELECT steam,name,score,kills,deaths
+      SELECT steam,name,score,kills,deaths,connected
       FROM rankme
       ORDER BY score DESC
     `);
